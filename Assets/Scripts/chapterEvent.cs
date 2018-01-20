8435a55f21e15aae87608d05736fbceb38c064ac
DeerRed <2420860654@qq.com>
2018-01-20 19:57:34 +0800

    add sqlite

--- Assets/Scripts/chapterEvent.cs
+++ Assets/Scripts/chapterEvent.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using Mono.Data.Sqlite;
+using System.Data;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,6 +13,60 @@ public class chapterEvent : MonoBehaviour {
     public Image chapter_title;
     public Image bg;
     public AudioSource bgm;
+    public Text txt;
+    public Image avatar;
+
+    private struct DisplayUnit
+    {
+        public string line;
+        public string avatar;
+        public string music;
+    }
+
+    private enum MainTitleStates
+    {
+        mtvoid = 0,
+        mttitle = 1,
+        mtchapter = 2,
+    };
+
+    private MainTitleStates state;
+    private MainTitleStates stateNext;
+    private IDbConnection dbconn;
+
+    private DisplayUnit GetLineFromDataBase()
+    {     
+       
+
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        int id = (int)(Random.value * 1000) %3+1;
+        string sqlQuery = "SELECT line, avatarpath, musicpath from colorless where id = " + id.ToString() + ";";
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+        string line = "";
+        string avatarpath = "";
+        string musicpath = "";
+        while (reader.Read())
+        {
+            line = reader.GetString(0);
+            avatarpath = reader.GetString(1);
+            musicpath = reader.GetString(2);
+            Debug.Log("line= " + line + "  avatarpath =" + avatarpath + "  musicpath =" + musicpath);
+        }
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;     
+        Debug.Log("test bug");
+
+        DisplayUnit ret = new DisplayUnit
+        {
+            line = line,
+            avatar =avatarpath,
+            music=musicpath
+        };
+        return ret;
+    }
 
     void OnEnable() {
         chapter_title.enabled = false;
@@ -19,19 +75,37 @@ public class chapterEvent : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        
+        string conn = "URI=file:" + Application.dataPath + "/DataBase/test.db";
+        Debug.Log(conn);
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open();
     }
 
     // Update is called once per frame
     int i=0;
     void Update(){    
-        if (Input.GetMouseButtonUp(0) == true){
+       if (Input.GetMouseButtonUp(0) == true){
             if (chapter_title.enabled == false) chapter_title.enabled = true;
             else {
                 bg.enabled = true;
                 txtBox.gameObject.active = true;
-                bgm.GetComponent<audioController>().PlayMusic(bgm,"Assets/BGM/1.mp3"); 
+                bgm.GetComponent<audioController>().PlayMusic(bgm,"Assets/BGM/1.mp3");
+                DisplayUnit dpu = GetLineFromDataBase();
+                txt.text = dpu.line;
+                avatar.sprite = Resources.Load(dpu.avatar, typeof(Sprite)) as Sprite;
+             //   PlayMusic(dpu.music);
             } 
         }
+
+     //   if (Input.GetMouseButtonUp(0) == true)
+    //       {
+          //  if ((state == MainTitleStates.mtchapter) && (stateNext == MainTitleStates.mtchapter))
+         //   {
+               // DisplayUnit dpu = GetLineFromDataBase();
+            //      txt.text = dpu.line;
+            
+        //    }
+//RefreshState();
+        //}
     }
 }
--- Assets/Scripts/titleController.cs
+++ Assets/Scripts/titleController.cs
@@ -27,44 +27,83 @@ public class titleController : MonoBehaviour {
     private MainTitleStates stateNext;
     private IDbConnection dbconn;
 
-    private DisplayUnit GetLineFromDataBase() {
-        IDbCommand dbcmd = dbconn.CreateCommand();
-        int id = (int)(UnityEngine.Random.value * 1000) % (2) + 1;
-        string sqlQuery = "SELECT line, avatarpath, musicpath from colorless where id = " + id.ToString() + ";";
-        dbcmd.CommandText = sqlQuery;
-        IDataReader reader = dbcmd.ExecuteReader();
-        string line = "";
-        string avatarpath = "";
-        string musicpath = "";
-        while (reader.Read()) {
-            line = reader.GetString(0);
-            avatarpath = reader.GetString(1);
-            musicpath = reader.GetString(2);
-            Debug.Log("line= " + line + "  avatarpath =" + avatarpath + "  musicpath =" + musicpath);
-        }
-        reader.Close();
-        reader = null;
-        dbcmd.Dispose();
-        dbcmd = null;
+    private void B()
+    {
+        string conn = "URI=file:" + Application.dataPath + "/DataBase/test.db";
+        Debug.Log(conn);
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open();
+        
+        
 
-        DisplayUnit ret = new DisplayUnit {
-            line = line,
-            avatar = avatarpath,
-            music = musicpath
-        };
+        IDbCommand cmd = dbconn.CreateCommand();
+        string sqlInsert = "insert into colorless values (NULL, \"a\", \"b\", \"c\", \"d\", \"e\", \"f\");";
+        cmd.CommandText = sqlInsert;
+        cmd.ExecuteNonQuery();Debug.Log("test bug3");
+        cmd.Dispose();
+        cmd = null;
+        Debug.Log("test bug2");
+        dbconn.Close();
+        dbconn = null;
+        return;
+}
+
+    private void A()
+    {
+       
+    }
+
+    private void C()
+    {
+        string conn = "URI=file:" + Application.dataPath + "/DataBase/test.db";
+        Debug.Log(conn);
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); 
+
+        IDbCommand dbcmd2 = dbconn.CreateCommand();
+        string sqlQuery2 = "SELECT line, avatarpath, musicpath from colorless;";
+        dbcmd2.CommandText = sqlQuery2;
+        IDataReader reader2 = dbcmd2.ExecuteReader();
+        string line2 = "";
+        string avatarpath2 = "";
+        string musicpath2 = "";
+        while (reader2.Read())
+        {
+            line2 = reader2.GetString(0);
+            avatarpath2 = reader2.GetString(1);
+            musicpath2 = reader2.GetString(2);
+            Debug.Log("line2= " + line2 + "  avatarpath2 =" + avatarpath2 + "  musicpath2 =" + musicpath2);
+        }
+        reader2.Close();
+        reader2 = null;
+        dbcmd2.Dispose();
+        dbcmd2 = null;
+        dbconn.Close();
+        dbconn = null;
+
+    }
+
+
+
+    private DisplayUnit GetLineFromDataBase() {
+         DisplayUnit ret = new DisplayUnit {
+             line = "",
+             avatar = "",
+             music = ""
+         };
+       
+        A();B();C();
         return ret;
     }
 
     // Use this for initialization
     void Start () {
-        string conn = "URI=file:" + Application.dataPath + "/DataBase/test.db";
-        Debug.Log(conn);
-        dbconn = (IDbConnection)new SqliteConnection(conn);
-        dbconn.Open();
+         DisplayUnit dpu = GetLineFromDataBase();
+
     }
 
-// Update is called once per frame
+    // Update is called once per frame
     void Update () {
-        DisplayUnit dpu = GetLineFromDataBase();
+        
     }
 }
