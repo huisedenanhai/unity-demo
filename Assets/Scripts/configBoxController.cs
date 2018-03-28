41862d46d61e0b3372cc4c196380de9851b6a7cb
DeerRed <2420860654@qq.com>
2018-03-29 01:02:19 +0800

    modify returnTitleButton

--- Assets/Scripts/configBoxController.cs
+++ Assets/Scripts/configBoxController.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class configBoxController : MonoBehaviour {
 
     public Canvas configBox;
+    public Canvas chapter1, chapter2, chapter3, chapter4;
+    public Canvas txtBox;
    
 	// Use this for initialization
 	void Start () {
@@ -16,9 +18,19 @@ public class configBoxController : MonoBehaviour {
     {
         configBox.gameObject.active = !configBox.gameObject.active;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    public void returnTitleButton()
+    {
+        chapter1.gameObject.active = false;
+        chapter2.gameObject.active = false;
+        chapter3.gameObject.active = false;
+        chapter4.gameObject.active = false;
+        txtBox.gameObject.active = false;
+        configBox.gameObject.active = !configBox.gameObject.active;
+    }
+
+    // Update is called once per frame
+    void Update () {
         if (Input.GetMouseButtonUp(1) == true) configBox.gameObject.active = false;
 	}
 }
